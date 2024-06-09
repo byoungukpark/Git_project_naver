0a960ef002dfb7cf25403b7b5e6a97055108a840
bu.k <88645300+byoungukpark@users.noreply.github.com>
2024-06-10 02:34:47 +0900

    Add files via upload

---
+++ pose_lstm/test01/p_1_1_get_pose_data.py
@@ -0,0 +1,185 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import os, time
+
+#이건 뭔데
+seq_length = 30
+speed = 0.03
+time_to_start = 2 # 초
+
+
+# 미디어 파이프 포즈 모델 정의??
+mp_pose = mp.solutions.pose
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+pose = mp_pose.Pose(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5)
+
+#카메라 키기
+cap = cv2.VideoCapture(0)
+
+#파일 만들기 
+os.makedirs('dataset', exist_ok=True)
+
+action, idx = None, None
+stop_=False
+
+a=0 # frame debug
+
+# print(f'1회 데이터 입력 시간: {secs_for_action}초')
+print(f'데이터 입력 시작 시 딜레이: {time_to_start}초')
+
+anounce_for_user = f'''
+웹캠 화면에서 메뉴 선택
+l 데이터를 쌓을 수어의 한글 단어 입력
+. 수어 데이터 입력 시작
+y 데이터 입력 준비 시간 변경(defalut: {time_to_start}s)
+ESC 종료
+'''
+print(anounce_for_user)
+
+while cap.isOpened():
+    ###
+    success, image = cap.read()
+    if not success:
+        print("Ignoring empty camera frame.\n웹캠을 사용중인 프로세스를 중지해주세요.")
+        continue
+
+    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = pose.process(image)
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    mp_drawing.draw_landmarks(
+        image,
+        results.pose_landmarks,
+        mp_pose.POSE_CONNECTIONS,
+        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+        # Flip the image horizontally for a selfie-view display.
+    
+    cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
+
+    key = cv2.waitKey(1)
+    #=====================여기까진 실행 가능 ==================
+
+
+    if key == ord('l'):
+        # 단어, 라벨 입력
+        action = input('단어 입력: ')
+        while True:
+            idx = input('라벨 입력: ')
+            try:
+                int_value = int(idx)
+                break
+            except ValueError:
+                print("정수 값을 입력해주세요.")
+        print(f'({action}, {idx}) 입력 완료')
+    
+
+    if key == ord('y'):
+        while True:
+            time_to_start = input(f'{time_to_start} -> ')
+            try:
+                time_to_start = float(time_to_start)
+                break
+            except ValueError:
+                print("실수 값을 입력해주세요.")
+        print('데이터 입력 준비 시간 변경 완료')
+        
+    if key == ord('.'):
+        if action is None or idx is None:
+            print("l을 눌러 입력할 단어를 설정해주세요.")
+            continue
+        print(f'({action}, {idx}): 데이터 생성을 {time_to_start}초 뒤 시작합니다.')
+        print('q: 중단(준비 시간 이후 중단 가능)')
+        data = []
+        ###
+        ret, img = cap.read()
+        img = cv2.flip(img, 1)
+        cv2.putText(img, f'Ready...', org=(10, 30), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0,0,0), thickness=2)
+        cv2.imshow('img', img)
+        cv2.waitKey(int(time_to_start*1000)) 
+
+        i=0
+        while i < seq_length:
+            time.sleep(speed)
+            a+=1
+            ret, img = cap.read()
+
+            img = cv2.flip(img, 1)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            result = pose.process(img)
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+               
+            #데이터 넣을 빈 배열
+            d = []
+
+            for k in range(33):
+                d.append(results.pose_landmarks.landmark[k].x)
+                d.append(results.pose_landmarks.landmark[k].y)
+                d.append(results.pose_landmarks.landmark[k].z)
+                d.append(results.pose_landmarks.landmark[k].visibility)
+            
+            d=np.concatenate([d, [idx]])
+            data.append(d)
+            i+=1
+            
+            cv2.imshow('img', img)
+            if cv2.waitKey(1) == ord('q'):
+                stop_=True
+                break
+        if stop_:
+            stop_=False
+            print("데이터 생성 중단")
+            continue
+        print("frame: ", a)
+        ###
+        data = np.array(data)
+        print(action, data.shape) #debug
+        
+
+
+        # 시퀀스 데이터 생성
+        full_seq_data = []
+        full_seq_data.append(data)
+
+        # list -> numpy
+        full_seq_data = np.array(full_seq_data)
+        print(action, full_seq_data.shape) # debug
+
+        # 저장할 npy 파일 이름
+        file_name = str(idx) + '_' + str(action) + '_s_0.npy'
+        
+        # 저장
+        script_directory = os.path.dirname(os.path.abspath(__file__))
+        save_data = os.path.join(script_directory, "dataset", file_name)
+        ##
+        save_file_num = 0
+        while os.path.exists(save_data):
+            save_file_num += 1
+            length_except_num = len(save_data.split('_')[-1])
+            save_data = save_data[:-length_except_num] # 'label_action_'
+            save_data += str(save_file_num) + '.npy'
+        ##
+        np.save(save_data, full_seq_data)
+
+        # 프레임 단위 데이터 저장 디렉토리
+        file_name = str(idx) + '_' + str(action) + '_s_'+str(save_file_num)+'.npy'
+        frame_data = os.path.join(script_directory, "dataset_frame", file_name) # 프레임 데이터
+        np.save(frame_data, data)   # 저장
+
+        print(f'({action}, {idx}):', data.shape, full_seq_data.shape, f'\n{file_name} 데이터 생성 완료')
+
+        
+        
+
+    if key == 27:  # ESC 키를 누르면 루프 종료
+        break
+    ###
+
+
+
---
+++ pose_lstm/test01/p_3_play.py
@@ -0,0 +1,148 @@
+import json
+import cv2
+import mediapipe as mp
+import numpy as np
+import os
+import base64
+import asyncio
+from tensorflow.keras.models import load_model
+
+
+mp_holistic = mp.solutions.holistic  # holistic: 얼굴, 손 등 감지
+
+def process_frame(image_data):
+    # base64 형식의 이미지 데이터를 bytes로 디코딩
+    image_bytes = base64.b64decode(image_data)
+    # bytes를 NumPy 배열로 변환
+    np_arr = np.frombuffer(image_bytes, np.uint8)
+    # NumPy 배열을 이미지로 변환
+    img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+    return img
+
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # COLOR CONVERSION BGR 2 RGB
+    image.flags.writeable = False  # 이미지 수정 불가 (결과 왜곡 방지?)
+    results = model.process(image)  # 모델을 사용해 입력 이미지에 대한 예측 수행
+    image.flags.writeable = True  # 이미지 다시 수정가능
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # COLOR COVERSION RGB 2 BGR
+    return image, results
+
+
+
+
+script_directory = os.path.dirname(os.path.abspath(__file__))
+dataset_directory = os.path.join(script_directory, "dataset")
+print("현재 작업 디렉토리:", script_directory)
+
+MODEL_PATH = os.path.join(script_directory,'final_model/model_ko.h5.keras')
+model = load_model(MODEL_PATH, compile=False) # 코랩 사용시 compile=False 필수
+
+data_file_list = os.listdir(dataset_directory)
+data_file_list = sorted(data_file_list, reverse=True)
+
+actions = {}
+for file_name in data_file_list:
+    parts = file_name.split("_")
+    if parts[1] not in actions:
+        actions[int(parts[0])] = parts[1]
+        # actions.append((parts[1], parts[0])) # 파일 목록(npy)에서 단어 추출
+
+print(actions)
+
+sentence_length = 10
+seq_length = 30
+
+
+
+
+
+
+#카메라 키기
+cap = cv2.VideoCapture(0)
+
+#기본 설정
+seq = []
+action_seq = [] 
+previous = '' # 이전 단어
+
+dc=0 # debug_count
+
+# 미디어 파이프 포즈 모델 정의??
+mp_pose = mp.solutions.pose
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+pose = mp_pose.Pose(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5)
+
+
+while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+        print("Ignoring empty camera frame.\n웹캠을 사용중인 프로세스를 중지해주세요.")
+        continue
+    image2 = image
+
+    image, result = mediapipe_detection(image, pose)
+
+    image2 = cv2.cvtColor(cv2.flip(image2, 1), cv2.COLOR_BGR2RGB)
+    image2.flags.writeable = False
+    results = pose.process(image2)
+    image2.flags.writeable = True
+    image2 = cv2.cvtColor(image2, cv2.COLOR_RGB2BGR)
+
+    mp_drawing.draw_landmarks(
+        image2,
+        results.pose_landmarks,
+        mp_pose.POSE_CONNECTIONS,
+        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+        # Flip the image horizontally for a selfie-view display.
+    
+    cv2.imshow('MediaPipe Pose', cv2.flip(image2, 1))
+    
+
+    if result.pose_landmarks is not None:
+        #데이터 넣을 빈 배열
+        d = []
+        for k in range(33):
+            d.append(result.pose_landmarks.landmark[k].x)
+            d.append(result.pose_landmarks.landmark[k].y)
+            d.append(result.pose_landmarks.landmark[k].z)
+            d.append(result.pose_landmarks.landmark[k].visibility)
+
+        seq.append(d)
+
+
+        dc+=1 
+        print(dc, "debug1.seq:", len(seq))
+
+        if len(seq) < seq_length: # 시퀀스 최소치가 쌓인 이후부터 판별
+            continue
+
+        if len(seq)>seq_length*100:  # 대충 한번씩 쌓인거 지움
+            seq=seq[-seq_length:]
+        
+        # 시퀀스 데이터를 신경망 모델에 입력으로 사용할 수 있는 형태로 변환
+        input_data = np.expand_dims(np.array(seq[-seq_length:], dtype=np.float32), axis=0)
+
+        y_pred = model.predict(input_data).squeeze() # 각 동작에 대한 예측 결과 (동작이 각 라벨의 동작일 확률 반환)
+
+        i_pred = int(np.argmax(y_pred)) # 최댓값 인덱스: 예측값이 가장 높은 값(동작)의 인덱스
+        conf = y_pred[i_pred] # 가장 확률 높은 동작의 확률이
+
+        # print("conf debug", conf)
+        if conf < 0.9:   # 70% 이상일 때만 수행
+            continue
+
+        # debug
+        print("debug2.예측동작:", i_pred)
+
+
+        action = actions[i_pred]
+        
+        if previous == action: continue  # 중복 전달 회피
+        previous = action
+
+        seq=[] # 초기화
+
